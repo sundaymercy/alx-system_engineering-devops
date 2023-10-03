d136eaabf823e5fa4dd54d41d3a146554ffc1732
sundaymercy <taiwomercy090@gmail.com>
2023-10-03 02:56:43 -0700

    puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,19 +1,34 @@
-# redirect me and nginx
-exec {'apt-get-update':
-  command => '/usr/bin/apt-get update'
+#!/usr/bin/env bash
+# Manifest to install and configure an Nginx server using Puppet instead of Bash
+package {'nginx':
+  ensure => 'present',
+}
+exec {'update apt-get and install':
+    command  => 'sudo apt-get update && sudo apt-get install nginx',
+    path     => '/usr/bin:/usr/sbin:/bin',
+    provider => shell
 }
 
-package {'apache2.2-common':
-  ensure  => 'absent',
-  require => Exec['apt-get-update']
+exec {'add landing page':
+    command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+    path     => '/usr/bin:/usr/sbin:/bin',
+    provider => shell
 }
 
-package { 'nginx':
-  ensure  => 'installed',
-  require => Package['apache2.2-common']
+exec {'redirection':
+  command  => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me \/ permanent;/" /etc/nginx/sites-available/default',
+  provider => shell,
 }
 
-service {'nginx':
-  ensure  =>  'running',
-  require => file_line['perform a redirection'],
+exec { 'allow firewall':
+    command => 'ufw allow "NGINX Full"',
+    path    => '/usr/bin:/usr/sbin:/bin',
+  provider  => shell,
+}
+
+exec { 'restart nginx':
+    command => 'sudo service nginx restart',
+    path    => '/usr/bin:/usr/sbin:/bin',
+  provider  => shell,
+
 }
