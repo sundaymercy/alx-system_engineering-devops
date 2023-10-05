fb719243a8d9dd90e590e695a377224714fa7031
sundaymercy <taiwomercy090@gmail.com>
2023-10-05 05:19:04 -0700

    response

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,13 @@
+# instal and configure nginx
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package { 'nginx':
+  ensure => installed,
+}
+-> file_line { 'header_served_by':
+  path  => '/etc/nginx/sites-available/default',
+  match => '^server {',
+  line  => "server {\n\tadd_header X-Served-By \"${hostname}\";",
+  multiple => false,
+
